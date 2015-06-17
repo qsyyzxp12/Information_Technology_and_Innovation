57212884a6b983e93d8ea87bdb7a2579fd76ea2b
qsyyzxp12 <b01705040@ntu.edu>
2015-06-17 18:01:50 +0800

    Nerds' War_showCompany.html

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 $(document).ready(function(){
+	$('.goButton').click(function(){
+		var price = $('.chooseCompDiv select').val() * 30;
+		var ret = confirm("您需付"+price+"元");
+		if(ret == true)
+			window.location.href='NerdsWar_showCompany.html';
+		else;
+
+	});
 });
